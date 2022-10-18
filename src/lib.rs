333e3b520ea135eb7b239214b9363cd6ac689218
Brandon Christoffer <me@brandonchristoffer.com>
2022-10-18 12:25:33 -0700

    Forgot to format, oops

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ impl<'a> Gpu<'a> {
 
     pub fn device(&self) -> Option<&Device> {
         if let Some(device) = &self.device {
-            return Some(device)
+            return Some(device);
         }
         None
     }
@@ -41,23 +41,38 @@ impl<'a> Gpu<'a> {
     }
 
     pub fn new_device(&mut self, queues: i32, flags: vk::QueueFlags) {
-        let queue_family_indices = self.properties.find_queue_families(flags).expect("queue family not found");
+        let queue_family_indices = self
+            .properties
+            .find_queue_families(flags)
+            .expect("queue family not found");
         let queue_priorities = vec![1.0; queues as usize];
-        let queue_create_infos = [
-            vk::DeviceQueueCreateInfo::builder()
-                .queue_family_index(queue_family_indices[0] as u32)  // first index will always be suitable
-                .queue_priorities(&queue_priorities)
-                .build()
-        ];
-        let enabled_layer_names = self.properties.layers.iter().map(|p| p.layer_name.as_ptr()).collect::<Vec<*const i8>>();
-        let enabled_extension_names = self.properties.extensions.iter().map(|p| p.extension_name.as_ptr()).collect::<Vec<*const i8>>();
+        let queue_create_infos = [vk::DeviceQueueCreateInfo::builder()
+            .queue_family_index(queue_family_indices[0] as u32) // first index will always be suitable
+            .queue_priorities(&queue_priorities)
+            .build()];
+        let enabled_layer_names = self
+            .properties
+            .layers
+            .iter()
+            .map(|p| p.layer_name.as_ptr())
+            .collect::<Vec<*const i8>>();
+        let enabled_extension_names = self
+            .properties
+            .extensions
+            .iter()
+            .map(|p| p.extension_name.as_ptr())
+            .collect::<Vec<*const i8>>();
         let enabled_features = &self.properties.features;
         let create_info = vk::DeviceCreateInfo::builder()
             .queue_create_infos(&queue_create_infos)
-            .enabled_layer_names(&enabled_layer_names)  // deprecated, but adding for compatibility
+            .enabled_layer_names(&enabled_layer_names) // deprecated, but adding for compatibility
             .enabled_extension_names(&enabled_extension_names)
             .enabled_features(enabled_features);
-        self.device = unsafe { self.instance.create_device(*self.handle, &create_info, None).ok() };
+        self.device = unsafe {
+            self.instance
+                .create_device(*self.handle, &create_info, None)
+                .ok()
+        };
     }
 }
 
@@ -73,12 +88,24 @@ pub struct GpuProperties {
 impl GpuProperties {
     pub fn new(instance: &Instance, physical_device: &vk::PhysicalDevice) -> Self {
         Self {
-            layers: unsafe { instance.enumerate_device_layer_properties(*physical_device).unwrap() },
-            extensions: unsafe { instance.enumerate_device_extension_properties(*physical_device).unwrap() },
+            layers: unsafe {
+                instance
+                    .enumerate_device_layer_properties(*physical_device)
+                    .unwrap()
+            },
+            extensions: unsafe {
+                instance
+                    .enumerate_device_extension_properties(*physical_device)
+                    .unwrap()
+            },
             features: unsafe { instance.get_physical_device_features(*physical_device) },
             properties: unsafe { instance.get_physical_device_properties(*physical_device) },
-            memory_properties: unsafe { instance.get_physical_device_memory_properties(*physical_device) },
-            queue_family_properties: unsafe { instance.get_physical_device_queue_family_properties(*physical_device) },
+            memory_properties: unsafe {
+                instance.get_physical_device_memory_properties(*physical_device)
+            },
+            queue_family_properties: unsafe {
+                instance.get_physical_device_queue_family_properties(*physical_device)
+            },
         }
     }
 
@@ -107,14 +134,15 @@ impl GpuProperties {
     }
 
     pub fn find_queue_families(&self, flags: vk::QueueFlags) -> Option<Vec<usize>> {
-        let indices = self.queue_family_properties()
+        let indices = self
+            .queue_family_properties()
             .iter()
             .enumerate()
             .filter(|(_, v)| (v.queue_flags.as_raw() & flags.as_raw()) != 0)
             .map(|(i, _)| i)
             .collect::<Vec<usize>>();
         if indices.len() == 0 {
-            return None
+            return None;
         }
         Some(indices)
     }
